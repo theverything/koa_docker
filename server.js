5e3366af9a58b0646c400e902ea24b6242627799
theverything <j3ffhorn@gmail.com>
2014-08-17 10:46:35 -0700

    add redis

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var koa = require('koa');
-var http = require('http');
+var router = require('koa-router');
+var app = module.exports = koa();
+var redis = require("redis");
+var parse = require('url').parse;
+var redisURI = parse(process.env.REDIS_1_PORT);
+var client = redis.createClient(redisURI.port, redisURI.hostname);
 
-var app = koa();
+var incr = function (key) {
+  return function (cb) {
+    client.incr(key, cb);
+  };
+};
 
-app.use(function *() {
-  this.body = "Hello World!";
+app.use(router(app));
+
+app.get('/', function *(next) {
+  var count = yield incr('counter');
+  this.body = 'This page has been visited ' + count + ' times.';
 });
 
-var app = module.exports = http.createServer(app.callback());
 app.listen(8080);
